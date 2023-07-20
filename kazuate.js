7af2c1a3b2d033ca061be5dc3662962dfc47e32c
G285072022 <228507@st.takushoku-u.ac.jp>
2023-07-20 16:57:44 +0900

    第5回課題

--- kazuate.js
+++ kazuate.js
@@ -3,37 +3,45 @@ console.log('答え (デバック用) : ' + kotae);
 
 let kaisu = 0;
 
-hantei();
-
-
-
+let btn = document.querySelector('#print');
+btn.addEventListener('click', hantei); 
 
 function hantei() {
+
+  
     
-    let yoso = 4;
+    
+  if(kaisu < 4){
     kaisu = kaisu + 1;
+  }
+
+  let n = document.querySelector('input[name="number"]');
+  let number = n.value;
 
    let span = document.querySelector('span#kaisu');
    span.textContent = kaisu;
-    if(kaisu < 3){
+
+   let span1 = document.querySelector('span#answer');
+   span1.textContent = number;
     
-    if(kotae === yoso) {
+   if(kaisu < 3){
+     if(kotae == number) {
       let p = document.querySelector('p#result');
         p.textContent = '正解です。おめでとう！';
-      } else if(kotae > yoso){
+      } else if(kotae > number){
         let p = document.querySelector('p#result');
         p.textContent = 'まちがい．答えはもっと大きいですよ';
-      } else if(kotae < yoso) {
+      } else if(kotae < number) {
         let p = document.querySelector('p#result');
         p.textContent = 'まちがい．答えはもっと小さいですよ';
       }
     }
 
     else if(kaisu === 3){
-        if(kotae === yoso) {
+        if(kotae == number) {
           let p = document.querySelector('p#result');
           p.textContent = '正解です。おめでとう！';
-          } else if(kotae !== yoso){
+          } else if(kotae !== number){
             let p = document.querySelector('p#result');
             p.textContent = 'まちがい。 残念でした。答えは '  + kotae + 'です。';
           }
